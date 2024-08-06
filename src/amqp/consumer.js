16f6758fc21329327cfb1c736487affcff5bd4f1
KNU-K <knukang334@gmail.com>
2024-08-06 19:04:30 +0900

    add : DB 로그 정보 저장 완료 '

--- src/amqp/consumer.js
+++ src/amqp/consumer.js
@@ -1,16 +1,44 @@
+const moment = require("moment");
+const { default: Container } = require("typedi");
+async function parseTimestamp(timestamp) {
+    // "2024-08-06 18:51:32:5132" 형식을 "2024-08-06 18:51:32.513"으로 변환
+    const match = timestamp.match(/^(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}):(\d{1,4})$/);
+    if (match) {
+        const datetimePart = match[1];
+        const milliseconds = match[2].slice(0, 3); // 밀리초는 최대 세 자리까지만 허용
+        return `${datetimePart}.${milliseconds}`;
+    }
+    return null;
+}
+
 async function startConsumer({ channel }) {
     try {
         channel.consume(
             "log_queue",
-            (msg) => {
+            async (msg) => {
                 if (msg !== null) {
                     const logMessage = msg.content.toString();
                     console.log("Received:", logMessage);
 
-                    // 메시지 처리 로직 (예: DB에 저장 등)
+                    // 로그 메시지를 데이터베이스에 삽입
+                    try {
+                        const pool = Container.get("pool");
+                        const logObject = JSON.parse(logMessage);
+                        let { node, timestamp, level, message } = logObject;
+                        timestamp = await parseTimestamp(timestamp);
+                        timestamp = moment(timestamp, ["YYYY-MM-DD HH:mm:ss.SSSS", moment.ISO_8601]).toISOString();
+                        console.log(timestamp);
+                        const query = `
+                            INSERT INTO logs (node, timestamp, level, message)
+                            VALUES ($1, $2, $3, $4)
+                        `;
+                        const values = [node, timestamp, level, message];
 
-                    // 메시지 처리 완료 후 확인
-                    channel.ack(msg);
+                        await pool.query(query, values);
+                        console.log("Log inserted into database");
+                    } catch (dbError) {
+                        console.error("Error inserting log into database:", dbError);
+                    }
                 }
             },
             {
