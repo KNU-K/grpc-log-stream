99a3552769ed3e722972276287628e5c8861f7ad
KNU-K <knukang334@gmail.com>
2024-08-09 23:12:31 +0900

    test : 부하 테스트 진행

---
+++ example/express-server2/grpc-transport/index.js
@@ -0,0 +1,32 @@
+const TransportStream = require("winston-transport");
+const grpcClient = require("./grpc-client");
+
+class GrpcTransport extends TransportStream {
+    constructor(opts) {
+        super(opts);
+    }
+
+    log(info, callback) {
+        setImmediate(() => {
+            this.emit("logged", info);
+        });
+
+        const { timestamp, level, message } = info;
+
+        // ANSI 색상 코드를 제거합니다.
+        const cleanLevel = level.replace(/\x1B\[[0-9;]*m/g, "");
+        const cleanMessage = message.replace(/\x1B\[[0-9;]*m/g, "");
+
+        grpcClient.SendLog({ node: "node2", timestamp, level: cleanLevel, message: cleanMessage }, (err, response) => {
+            if (err) {
+                console.error("Failed to send log to gRPC server:", err);
+            } else {
+                console.log("Log sent to gRPC server:", response);
+            }
+        });
+
+        callback();
+    }
+}
+
+module.exports = GrpcTransport;
